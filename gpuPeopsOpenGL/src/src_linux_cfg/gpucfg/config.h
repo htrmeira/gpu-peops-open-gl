6b37157435edb8467ff376e269672bcbe54a041d
Heitor Meira <heitor@nextepi.com>
2014-04-13 23:21:34 -0300

    Initial commit

---
+++ gpuPeopsOpenGL/src/src_linux_cfg/gpucfg/config.h
@@ -0,0 +1,25 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you need to in order for stat and other things to work.  */
+/* #undef _POSIX_SOURCE */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* #undef ENABLE_NLS */
+/* #undef HAVE_CATGETS */
+/* #undef HAVE_GETTEXT */
+/* #undef HAVE_LC_MESSAGES */
+/* #undef HAVE_STPCPY */
+/* #undef HAVE_LIBSM */
+/* #undef PACKAGE_LOCALE_DIR */
+#define PACKAGE_DATA_DIR "/usr/local/share/gpucfg"
+#define PACKAGE_SOURCE_DIR "/home/pete/projects/gpucfg"
+
+/* Name of package */
+#define PACKAGE "gpucfg"
+
+/* Version number of package */
+#define VERSION "0.1"
+
